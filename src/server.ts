31516384dc5e55e67d2731f00122e6e842d9fd16
EmmanuelEmp <emmyochogwu@gmail.com>
2025-01-17 13:12:05 +0100

    docs

--- src/server.ts
+++ src/server.ts
@@ -8,36 +8,34 @@ import cookieParser from "cookie-parser";
 import swaggerUi from "swagger-ui-express";
 import swaggerJsdoc from "swagger-jsdoc";
 import swaggerDocs from "./utils/swagger"; 
+
 dotenv.config();
 
 connectDB();
 
 const app: Express = express();
 
-
 const PORT: number = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
-
 
 app.use(express.json());
 app.use(cookieParser());
 
-
+//Swagger documentation setup
 swaggerDocs(app, PORT);
+
+
+app.use("/api/users", userRoutes);
+app.use("/api/todo", todoRoutes);
 
 // Redirect root URL ("/") to Swagger UI ("/docs")
 app.get("/", (req: Request, res: Response) => {
   res.redirect("/docs");
 });
 
-// API routes
-app.use("/api/users", userRoutes); 
-app.use("/api/todo", todoRoutes);
-
-// Error handling middleware
 app.use(notFound);
 app.use(errorHandler);
 
-// Start the server
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
